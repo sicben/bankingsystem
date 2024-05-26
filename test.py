c4d78de829a7de3ad6d2c787bfc82a65fbecbbdb
sicben <50627063@qq.com>
2024-05-26 13:40:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ from bank import Bank
 if __name__ == '__main__':
     # 创建一个银行对象
     bank = Bank("测试银行")
-    bank.banking('001','T',10,'002')
+    bank.banking('001','T',10,'002')   #参数： banking(原账户，操作类型[D,W,T],金额，目标账户（转账使用，其他状态可以为None或‘’）））
